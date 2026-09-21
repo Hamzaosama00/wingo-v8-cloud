6b45e2aede27d61f962ef506509b466bc9600694
ItsBlaze2 <114988253+Hamzaosama00@users.noreply.github.com>
2026-09-21 07:45:37 +0500

    Change MIN_HISTORY default value from 50 to 10

--- backend/app.py
+++ backend/app.py
@@ -32,7 +32,7 @@ HISTORY_LIMIT = int(os.getenv("HISTORY_LIMIT", "500"))
 BACKFILL_TARGET = int(os.getenv("BACKFILL_TARGET", "500"))
 BACKFILL_PAGE_SIZE = int(os.getenv("BACKFILL_PAGE_SIZE", "100"))
 POLL_SECONDS = float(os.getenv("POLL_SECONDS", "3"))
-MIN_HISTORY = int(os.getenv("MIN_HISTORY", "50"))
+MIN_HISTORY = int(os.getenv("MIN_HISTORY", "10"))
 NUMBER_STATES = list(range(10))
 
 COLOR_STATES = ["red", "green", "violet"]
